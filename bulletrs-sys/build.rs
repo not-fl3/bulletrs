ee6992755fe6a61ff5a6747b53f9806d5db9fa16
fedor <not.fl3@gmail.com>
2017-10-09 13:49:44 +0300

    WIP macos build

--- bulletrs-sys/build.rs
+++ bulletrs-sys/build.rs
@@ -9,10 +9,9 @@ use std::env;
 fn main() {
     if env::var("TARGET").unwrap().contains("windows") {
         build_windows::build_windows();
-    }
-    else if env::var("TARGET").unwrap().contains("linux") {
+    } else if env::var("TARGET").unwrap().contains("linux") {
         build_linux::build_linux();
-    } if env::var("TARGET").unwrap().contains("linux") {
+    } else if env::var("TARGET").unwrap().contains("mac") {
         build_macos::build_macos();
     } else {
         panic!("Unsupported platform!");
