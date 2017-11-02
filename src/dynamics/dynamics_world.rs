5b99f9d822e4ed3ccf2b15e7187a329f0ef0359c
fedor <not.fl3@gmail.com>
2017-11-02 15:37:06 +0300

    Raytest

--- src/dynamics/dynamics_world.rs
+++ src/dynamics/dynamics_world.rs
@@ -80,7 +80,174 @@ impl DynamicsWorld {
                     max_sub_steps,
                     fixed_time_step,
                 );
-            }
+            },
         }
+    }
+
+    pub fn raytest<C>(&self, mut callback: C) -> C
+    where
+        C: RayResultCallback + InternalRayResultCallback,
+    {
+        let from = callback.world_from();
+        let to = callback.world_to();
+        match self {
+            &DynamicsWorld::Discrete { ref world, .. } => unsafe {
+                sys::btCollisionWorld_rayTest(
+                    world as *const _ as *mut _,
+                    &from as *const _,
+                    &to as *const _,
+                    callback.as_ptr(),
+                )
+            },
+        }
+        callback
+    }
+}
+
+pub struct RayIntersection {
+    pub fraction: f64,
+    pub normal: Vector3<f64>,
+    pub point: Vector3<f64>,
+}
+
+/// Internal and unsafe methods.
+/// Not exported so can't be imported and used.
+pub trait InternalRayResultCallback {
+    fn world_from(&self) -> sys::btVector3;
+    fn world_to(&self) -> sys::btVector3;
+    fn as_ptr(&mut self) -> *mut sys::btCollisionWorld_RayResultCallback;
+}
+
+pub trait RayResultCallback {
+    fn intersections(&self) -> Vec<RayIntersection>;
+}
+
+pub struct AllRayResultCallback {
+    callback: sys::btCollisionWorld_AllHitsRayResultCallback,
+}
+
+impl AllRayResultCallback {
+    pub fn new<T, T1>(from: T, to: T1) -> Self
+    where
+        T: Into<Vector3<f64>>,
+        T1: Into<Vector3<f64>>,
+    {
+        let from: BulletVector3 = from.into().into();
+        let to: BulletVector3 = to.into().into();
+
+        AllRayResultCallback {
+            callback: unsafe {
+                sys::btCollisionWorld_AllHitsRayResultCallback::new(
+                    &from.0 as *const _ as *const _,
+                    &to.0 as *const _ as *const _,
+                )
+            },
+        }
+    }
+}
+
+impl InternalRayResultCallback for AllRayResultCallback {
+    fn world_from(&self) -> sys::btVector3 {
+        self.callback.m_rayFromWorld.clone()
+    }
+    fn world_to(&self) -> sys::btVector3 {
+        self.callback.m_rayToWorld.clone()
+    }
+
+    fn as_ptr(&mut self) -> *mut sys::btCollisionWorld_RayResultCallback {
+        &mut self.callback as *mut _ as *mut _
+    }
+}
+
+impl RayResultCallback for AllRayResultCallback {
+    fn intersections(&self) -> Vec<RayIntersection> {
+        let normals = unsafe {
+            ::std::slice::from_raw_parts(
+                self.callback.m_hitNormalWorld.m_data,
+                self.callback.m_hitNormalWorld.m_size as usize,
+            )
+        };
+        let points = unsafe {
+            ::std::slice::from_raw_parts(
+                self.callback.m_hitPointWorld.m_data,
+                self.callback.m_hitPointWorld.m_size as usize,
+            )
+        };
+        let fractions = unsafe {
+            ::std::slice::from_raw_parts(
+                self.callback.m_hitFractions.m_data,
+                self.callback.m_hitFractions.m_size as usize,
+            )
+        };
+        let _objects = unsafe {
+            ::std::slice::from_raw_parts(
+                self.callback.m_collisionObjects.m_data,
+                self.callback.m_collisionObjects.m_size as usize,
+            )
+        };
+
+        let mut intersections = vec![];
+
+        for i in 0..normals.len() {
+            intersections.push(RayIntersection {
+                fraction: fractions[i],
+                normal: Vector3::from_slice(&normals[i].m_floats[0..3]),
+                point: Vector3::from_slice(&points[i].m_floats[0..3]),
+            });
+        }
+        intersections
+    }
+}
+
+pub struct ClosestRayResultCallback {
+    callback: sys::btCollisionWorld_ClosestRayResultCallback,
+}
+
+
+impl ClosestRayResultCallback {
+    pub fn new<T, T1>(from: T, to: T1) -> Self
+    where
+        T: Into<Vector3<f64>>,
+        T1: Into<Vector3<f64>>,
+    {
+        let from: BulletVector3 = from.into().into();
+        let to: BulletVector3 = to.into().into();
+
+        ClosestRayResultCallback {
+            callback: unsafe {
+                sys::btCollisionWorld_ClosestRayResultCallback::new(
+                    &from.0 as *const _ as *const _,
+                    &to.0 as *const _ as *const _,
+                )
+            },
+        }
+    }
+
+    pub fn closest_hit_fraction(&self) -> f64 {
+        self.callback._base.m_closestHitFraction
+    }
+}
+
+impl InternalRayResultCallback for ClosestRayResultCallback {
+    fn world_from(&self) -> sys::btVector3 {
+        self.callback.m_rayFromWorld.clone()
+    }
+    fn world_to(&self) -> sys::btVector3 {
+        self.callback.m_rayToWorld.clone()
+    }
+    fn as_ptr(&mut self) -> *mut sys::btCollisionWorld_RayResultCallback {
+        &mut self.callback as *mut _ as *mut _
+    }
+}
+
+impl RayResultCallback for ClosestRayResultCallback {
+    fn intersections(&self) -> Vec<RayIntersection> {
+        vec![
+            RayIntersection {
+                fraction: self.callback._base.m_closestHitFraction,
+                point: Vector3::from_slice(&self.callback.m_hitPointWorld.m_floats[0..3]),
+                normal: Vector3::from_slice(&self.callback.m_hitNormalWorld.m_floats[0..3]),
+            },
+        ]
     }
 }
